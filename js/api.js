04cdc3686d028a5b5bedbd8af570f167584fbd29
Renata <renataav.rv@gmail.com>
2026-02-05 21:48:42 -0300

    submetendo o formulario para criar um novo dado

--- js/api.js
+++ js/api.js
@@ -10,7 +10,25 @@ const api = {
             alert('Erro ao buscar pensamentos')
             throw error
         }
+    },
+
+    async salvarPensamento(pensamento){
+        try{
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "applocation/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+            return await response.json()
+        }
+        catch{
+            alert('Erro ao buscar pensamentos')
+            throw error
+        }
     }
+
 }
 
 export default api;
--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 // responsavel pela lógica principal do carregamento da aplicação
 
 import  ui  from "./ui.js";
+import api from "./api.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     ui.renderizarPensamentos()
+
+    const formularioPensamento = document.getElementById("pensamento-form")
+    formularioPensamento.addEventListener("subimit", manipularSubmissaoFormulario)
 })
+
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault();
+    const id = document.getElementById("pensamento-id").value
+    const conteudo = document.getElementById("pensamento-conteudo").value
+    const autoria = document.getElementById("pensamento-autoria").value
+
+    try{
+        await api.salvarPensamento({conteudo, autoria})
+        ui.renderizarPensamentos()
+    }
+    catch{
+        alert("Erro ao salvar pensamento")
+    }
+
+}
